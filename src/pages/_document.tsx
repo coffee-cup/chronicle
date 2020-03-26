27b06608fb222af9af1aea8e355299c232baa3dd
Jake Runzer <jakerunzer@gmail.com>
2020-03-26 14:10:03 +0000

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,6 +20,31 @@ class MyDocument extends Document {
           <noscript>
             <img src="https://api.simpleanalytics.io/hello.gif" alt="" />
           </noscript>
+
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="96x96"
+            href="/favicon-96x96.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
 
         <body>

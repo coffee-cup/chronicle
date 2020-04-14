fa7b1bb75a34d4d2af0a3d965e1c9bdead39d4ec
Jake Runzer <jakerunzer@gmail.com>
2020-04-14 17:37:34 +0100

    show spinner when waiting for result from twitter login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,7 +2,16 @@
 import { useRouter } from "next/router";
 import * as React from "react";
 import { useForm } from "react-hook-form";
-import { Box, Button, Flex, Input, jsx, Label, Styled } from "theme-ui";
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  jsx,
+  Label,
+  Styled,
+  Spinner,
+} from "theme-ui";
 import Layout from "../components/Layout";
 import Link from "../components/Link";
 import useUser from "../hooks/use-user";
@@ -15,7 +24,7 @@ interface FormData {
 }
 
 const Login = () => {
-  const { login } = useUser();
+  const { login, cameFromTwitter } = useUser();
   const router = useRouter();
 
   const { register, handleSubmit, watch, errors } = useForm<FormData>();
@@ -42,54 +51,62 @@ const Login = () => {
           width: "100%",
         }}
       >
-        <Box
-          sx={{
-            width: "100%",
-            maxWidth: [null, "measure"],
-          }}
-        >
-          <Styled.h1>Login to Chronicle</Styled.h1>
-          <Box as="form" onSubmit={onSubmit}>
-            <Box>
-              {error && <Box sx={{ color: "red", pb: 2 }}>{error}</Box>}
-
-              <Label htmlFor="email">email</Label>
-              {errors.email && errors.email.type === "pattern" && (
-                <span sx={{ color: "error", fontSize: 1 }}>invalid email</span>
-              )}
-              <Input
-                name="email"
-                mb={3}
-                placeholder="hello@gmail.com"
-                className={errors.email ? "error" : ""}
-                ref={register({ required: true, pattern: emailRegex })}
-              />
-
-              <Label htmlFor="password">password</Label>
-              {errors.password && errors.password.type === "minLength" && (
-                <span sx={{ color: "error", fontSize: 1 }}>
-                  must be 8 characters long
-                </span>
-              )}
-              <Input
-                type="password"
-                name="password"
-                mb={3}
-                placeholder="something secure"
-                className={errors.password ? "error" : ""}
-                ref={register({ required: true, minLength: 8 })}
-              />
-            </Box>
-            <p>
-              New to Chronicle? <Link to="/signup">Create an account</Link>.
-            </p>
-
-            <Flex sx={{ alignItems: "center", flexWrap: "wrap" }}>
-              <Button sx={{ mr: 2 }}>Login with email</Button> or{" "}
-              <LoginWithTwitter sx={{ ml: [0, 2] }} />
-            </Flex>
+        {cameFromTwitter ? (
+          <Box>
+            <Spinner />
           </Box>
-        </Box>
+        ) : (
+          <Box
+            sx={{
+              width: "100%",
+              maxWidth: [null, "measure"],
+            }}
+          >
+            <Styled.h1>Login to Chronicle</Styled.h1>
+            <Box as="form" onSubmit={onSubmit}>
+              <Box>
+                {error && <Box sx={{ color: "red", pb: 2 }}>{error}</Box>}
+
+                <Label htmlFor="email">email</Label>
+                {errors.email && errors.email.type === "pattern" && (
+                  <span sx={{ color: "error", fontSize: 1 }}>
+                    invalid email
+                  </span>
+                )}
+                <Input
+                  name="email"
+                  mb={3}
+                  placeholder="hello@gmail.com"
+                  className={errors.email ? "error" : ""}
+                  ref={register({ required: true, pattern: emailRegex })}
+                />
+
+                <Label htmlFor="password">password</Label>
+                {errors.password && errors.password.type === "minLength" && (
+                  <span sx={{ color: "error", fontSize: 1 }}>
+                    must be 8 characters long
+                  </span>
+                )}
+                <Input
+                  type="password"
+                  name="password"
+                  mb={3}
+                  placeholder="something secure"
+                  className={errors.password ? "error" : ""}
+                  ref={register({ required: true, minLength: 8 })}
+                />
+              </Box>
+              <p>
+                New to Chronicle? <Link to="/signup">Create an account</Link>.
+              </p>
+
+              <Flex sx={{ alignItems: "center", flexWrap: "wrap" }}>
+                <Button sx={{ mr: 2 }}>Login with email</Button> or{" "}
+                <LoginWithTwitter sx={{ ml: [0, 2] }} />
+              </Flex>
+            </Box>
+          </Box>
+        )}
       </Flex>
     </Layout>
   );

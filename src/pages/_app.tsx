507ade8427a15cf3e94c64c3c851a8bd5fc3b244
Jake Runzer <jakerunzer@gmail.com>
2020-12-30 13:21:59 -0700

    add fathom analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,15 @@ import { Styled, ThemeProvider } from "theme-ui";
 import theme from "../styles";
 import { UserProvider } from "../hooks/use-user";
 import * as Sentry from "@sentry/node";
+import { useFathom } from "../hooks/useFathom";
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useFathom("BBAISTHM", "chronicle.ink");
+
   return (
     <ThemeProvider theme={theme}>
       <UserProvider>
